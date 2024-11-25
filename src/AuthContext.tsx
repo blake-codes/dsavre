d18b9e1cba88f2fda736b6d8f36be5e5da3266c4
blake-codes <blakeharrison4u@gmail.com>
2024-11-25 13:29:47 +0100

    fix

--- src/AuthContext.tsx
+++ src/AuthContext.tsx
@@ -29,6 +29,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = (): AuthContextType => {
   const context = useContext(AuthContext);
   if (!context) {
